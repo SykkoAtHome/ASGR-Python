70e289863964963657c6442d0ccbbba7d96ed767
SykkoAtHome <rafal@tluchowski.pl>
2023-06-19 00:36:09 +0200

    new feature: EventLogger

--- models.py
+++ models.py
@@ -12,7 +12,7 @@ class Users(Base):
     last_name = Column(String)
     hashed_password = Column(String)
     user_type = Column(Integer, ForeignKey("user_type.id"), default=3)
-    create_date = Column(TIMESTAMP, default=datetime.datetime.utcnow)
+    create_date = Column(TIMESTAMP, default=datetime.datetime.now)
     email_confirmed = Column(Boolean, default=False)
     is_active = Column(Boolean, default=True)
 
@@ -22,20 +22,50 @@ class UserType(Base):
     id = Column(Integer, primary_key=True, index=True)
     user_type = Column(String)
 
-# class Game(Base):
-#     __tablename__ = 'games'
-#     id = Column(Integer, primary_key=True, index=True)
-#     owner_id = Column(Integer, ForeignKey("users.id"))
-#     directions_point = Column(String)
-#     game_type = Column(Integer, ForeignKey("game_types.id"))
-#     scheduled_start = Column(DateTime(timezone=True))
-#     game_duration = Column(Integer)
-#     max_players = Column(Integer)
-#     created_date = Column(DateTime(timezone=True), server_default=func.now())
-#     is_active = Column(Boolean, default=False)
-#
-# class GameType(Base):
-#     __tablename__ = 'game_types'
-#     id = Column(Integer, primary_key=True, index=True)
-#     type_name = Column(String)
-#     type_description = Column(String)
+
+class Game(Base):
+    __tablename__ = 'games'
+    id = Column(Integer, primary_key=True, index=True)
+    owner_id = Column(Integer, ForeignKey("users.id"))
+    directions_point = Column(String)
+    game_type = Column(Integer, ForeignKey("game_type.id"))
+    game_name = Column(String)
+    game_description = Column(String)
+    scheduled_start = Column(TIMESTAMP)
+    auto_start = Column(Boolean, default=False)
+    game_duration = Column(Integer)
+    max_players = Column(Integer)
+    teams = Column(Integer, default=0)
+    created_date = Column(TIMESTAMP, default=datetime.datetime.now)
+    tick_interval = Column(Integer, default=30)
+    warmup = Column(Integer, default=10)
+    game_current_state = Column(Integer, ForeignKey("game_state.id"))
+
+
+class GameType(Base):
+    __tablename__ = 'game_type'
+    id = Column(Integer, primary_key=True, index=True)
+    type_name = Column(String)
+    type_description = Column(String)
+
+
+class GameState(Base):
+    __tablename__ = 'game_state'
+    id = Column(Integer, primary_key=True, index=True)
+    state_name = Column(String)
+    state_description = Column(String)
+
+
+class EventLogger(Base):
+    __tablename__ = 'event_logs'
+    id = Column(Integer, primary_key=True, index=True)
+    event_type = Column(Integer, ForeignKey("events.id"))
+    user_id = Column(Integer, ForeignKey("users.id"))
+    details = Column(String)
+    timestamp = Column(TIMESTAMP, default=datetime.datetime.now)
+
+
+class Events(Base):
+    __tablename__ = 'events'
+    id = Column(Integer, primary_key=True, index=True)
+    event_name = Column(String)
--- routers/game.py
+++ routers/game.py
@@ -5,7 +5,8 @@ from fastapi import APIRouter, Depends, HTTPException, Path
 from starlette import status
 from database import SessionLocal
 from .auth import get_current_user
-from models import Users
+from models import Game, EventLogger
+import datetime
 
 router = APIRouter(
     prefix='/game',
@@ -24,3 +25,52 @@ def get_db():
 db_dependency = Annotated[Session, Depends(get_db)]
 user_dependency = Annotated[dict, Depends(get_current_user)]
 
+
+class CreateGameRequest(BaseModel):
+    game_name: str = Field(max_length=24, default='Short game name')
+    game_description: str = Field(max_length=240, default='Game description')
+    directions_point: str = Field(default='(52.01958,20.98463)')
+    game_type: int = Field(gt=0, default=1)
+    scheduled_start: str = Field(default='2023-12-24 18:00:00')
+    game_duration: int = Field(gt=4, default=10, )
+    max_players: int = Field(lt=32, default=8)
+    teams: int = Field(lt=6)
+    warmup: int = Field(gt=0, default=10)
+
+
+@router.post("/", status_code=status.HTTP_201_CREATED)
+async def create_game(user: user_dependency, db: db_dependency,
+                      create_game_request: CreateGameRequest):
+    if user is None:
+        raise HTTPException(status_code=401, detail='Unauthorized')
+    if user.get('is_active') == 0:
+        raise HTTPException(status_code=403, detail='Forbidden')
+    try:
+        create_game_model = Game(owner_id=user.get('id'),
+                                 game_name=create_game_request.game_name,
+                                 game_description=create_game_request.game_description,
+                                 directions_point=create_game_request.directions_point,
+                                 game_type=create_game_request.game_type,
+                                 scheduled_start=create_game_request.scheduled_start,
+                                 game_duration=create_game_request.game_duration,
+                                 max_players=create_game_request.max_players,
+                                 teams=create_game_request.teams,
+                                 tick_interval=15,
+                                 warmup=create_game_request.warmup,
+                                 game_current_state=1
+                                 )
+        db.add(create_game_model)
+        db.flush()
+    except:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail='Error creating game')
+    else:
+        new_game_id = db.query(Game.id).filter(Game.game_name == create_game_request.game_name).first()
+        event_log = EventLogger(event_type=9,
+                                user_id=user.get('id'),
+                                details=f"New game: {create_game_request.game_name}. "
+                                        f"Created by username: {user.get('username')}, "
+                                        f"user_id: {user.get('id')}, "
+                                        f"game_id: {new_game_id}"
+                                )
+        db.add(event_log)
+        db.commit()
